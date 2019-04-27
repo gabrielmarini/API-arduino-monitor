598eb05ca76ec6a4e47aed78e877919c5690c021
Gabriel Marini <gabriel.g.marini@hotmail.com>
2019-04-27 14:59:32 -0300

    Inicio do projeto

---
+++ src/test/java/br/com/gmtechnology/api/arduinomonitor/ArduinoMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.gmtechnology.api.arduinomonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArduinoMonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
